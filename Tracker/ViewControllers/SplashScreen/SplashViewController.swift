8d08c76082677a4d1414c212a0e1a821aab46480
Mukhammed <Muhammed.Nurmuhanov@mail.ru>
2025-10-23 17:18:04 +0500

    develope(newFeature): Добавлена онбординговая страница.
    Выполнены следующие работы:
    1) Добавлен OnboardingViewController для отображения онбординговой страницы с помощью UIPageViewController и UIPageControl;
    2) В качестве подстраниц для OnboardingViewController были созданы FirstOnboardingViewController, SecondOnboardingViewController с соответствующим дизайном из макета figma.

--- Tracker/ViewControllers/SplashScreen/SplashViewController.swift
+++ Tracker/ViewControllers/SplashScreen/SplashViewController.swift
@@ -17,9 +17,18 @@ final class SplashViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        showTabBarController()
+        
+        let hasLaunchedBefore = UserDefaults.standard.bool(forKey: "hasLaunchedBefore")
+
+        if !hasLaunchedBefore {
+            UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
+            print("Первый запуск приложения")
+            showOnboardingViewController()
+        } else {
+            print("Повторный запуск приложения")
+            showTabBarController()
+        }
     }
-    
     
     private func showTabBarController() {
         let tabBarController = TabBarController()
@@ -27,6 +36,15 @@ final class SplashViewController: UIViewController {
         if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
            let window = windowScene.windows.first {
             window.setRoot(tabBarController, animated: true)
+        }
+    }
+    
+    private func showOnboardingViewController() {
+        let onboardingViewController = OnboardingViewController()
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            window.setRoot(onboardingViewController, animated: true)
         }
     }
     
